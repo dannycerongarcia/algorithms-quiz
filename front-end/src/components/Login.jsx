f594bfe2ddb0813e1f4d3eb85105c6a4a07dff9d
dannyceron94 <dannyceron94@gmail.com>
2020-09-15 03:10:44 -0700

    changes to login front-end

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -4,12 +4,17 @@ import './CSS/footer.css'
 import './CSS/logos.css'
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import GoogleMapReact from 'google-map-react';
 // importing redux variables and function
 import { setEmail, setIsLoggedIn } from '../redux/actions/userActions';
 import axios from 'axios';
 import md5 from 'md5';
 
 const Login = ({ dispatch, email, isLoggedIn }) => {
+
+    function createMarkup() {
+        return { __html: '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d405117.53148505!2d-122.21558399999996!3d37.50837300000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fa03953f35ee1%3A0xb8e47b9b0baa6a6d!2sRedwood%20City%2C%20CA!5e0!3m2!1sen!2sus!4v1600159265312!5m2!1sen!2sus" width="70%" height="400" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>' };
+    }
 
     let [myPassword, setPassword] = useState('');
     const updatePassword = (password) => {
@@ -59,14 +64,36 @@ const Login = ({ dispatch, email, isLoggedIn }) => {
     return (
 
         <div className="wallpaper">
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <h1>Algorithms quiz</h1>
-                    </div>
+            {/* <div className="container"> */}
+
+            <div>
+                {/* <!-- A grey horizontal navbar that becomes vertical on small screens --> */}
+                <nav class="navbar navbar-expand-sm bg-dark navbar-dark round-nav">
+
+                    {/* <!-- Links --> */}
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">HOME</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#about">ABOUT</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#contact">CONTACT</a>
+                        </li>
+                    </ul>
+
+                </nav>
+            </div>
+
+            <div className="row">
+                <div className="col">
+                    <h1>Algorithms quiz</h1>
                 </div>
+            </div>
+            <a name="top">
                 <div className="row">
-                    <div className="col-1"></div>
+                    <div className="col-2"></div>
                     <div className="col">
                         <div className="Black-tranparent-box">
                             <h2 className="sign-in-text">Sign in</h2>
@@ -111,25 +138,61 @@ const Login = ({ dispatch, email, isLoggedIn }) => {
 
                         </div>
                     </div>
-                    <div className="col-1"></div>
+                    <div className="col-2"></div>
                 </div>
+            </a>
+            {/* </div> */}
+            <div className="about-backgroun">
+                <a name="about">
+                    <div className="row">
+                        <div className="col-sm-0 col-lg-3">
+                        </div>
+                        <div className="col-sm-5 col-lg-3 align-text-left">
+                            <br />
+                            <br />
+                            <h1>ABOUT</h1>
+                            <div className="underline-yellow"></div>
+                            <br />
+                            <p>Algorithms Quiz is design to help to test and improve users skills in Algorithms</p>
+                            <p>Algorithms Quiz is design to help to test and improve users skills in Algorithms</p>
+                            
+                        </div>
+                        <div className="col-sm-5 col-lg-3">
+                            <p>some stuff </p>
+                        </div>
+                    </div>
+                </a>
             </div>
+            <div className="row" className="yellow-bar"></div>
+
+            <a name="contact">
+                <div className="row white-background">
+                    <div className="col"><h1 className="center-text" >CONTACT</h1>
+                        <div className="underline-yellow-center"></div>
+                        <br />
+                    </div>
+                </div>
+            </a>
+            <div className="row white-background"><div className="col">
+                <div dangerouslySetInnerHTML={createMarkup()} />
+            </div></div>
             <div className="login-footer">
                 {/* need a better looking footer */}
                 <div className="row">
-                    <div className="col-lg-4 col-sm-6">
-                        <h6>CONTACT</h6>
+                    <div className="col-lg-3 col-sm-6 align-text-left">
+
                         <p>Author: Danny Daneth Ceron Garcia.</p>
                         <p>EMAIL: <a href="mailto: dannycerongarcia@gmail.com?subject = Feedback&body = Message">dannycerongarcia@gmail.com</a></p>
                     </div>
-                    <div className="col-lg-8 col-sm-6">
+                    <div className="col-lg-9 col-sm-6">
                         <div className="row">
                             <a href="https://github.com/dannycerongarcia"><div className="dark-github"></div></a>
-                            <a href="https://www.linkedin.com/in/danny-ceron-garcia-1a3553103?challengeId=AQFWhidgrZO1OAAAAXN1knuQKlD_DzHR-4047_hddn46K33CbQwNqTEyf-z_airUyJydnxzNI_rabio5wRQxhkzNz8ESRsVH7A&submissionId=bd72ed4b-c204-2416-9872-cac84400c22e"><div className="linked-in"/></a>
+                            <a href="https://www.linkedin.com/in/danny-ceron-garcia-1a3553103?challengeId=AQFWhidgrZO1OAAAAXN1knuQKlD_DzHR-4047_hddn46K33CbQwNqTEyf-z_airUyJydnxzNI_rabio5wRQxhkzNz8ESRsVH7A&submissionId=bd72ed4b-c204-2416-9872-cac84400c22e"><div className="linked-in" /></a>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
     )
 }
